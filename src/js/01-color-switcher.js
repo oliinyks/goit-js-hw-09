1708bd8edfda663435efc411654f6ba5b828e050
Olijnyks <olijnyks@ukr.net>
2022-08-08 11:57:19 +0300

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,27 @@
+const buttonStart = document.querySelector('[data-start]');
+const buttonStop = document.querySelector('[data-stop]');
 
+buttonStop.disabled = true;
+
+buttonStart.addEventListener('click', onButtonStartClick);
+buttonStop.addEventListener('click', onButtonStopClick);
+
+function onButtonStartClick() {
+	buttonStart.disabled = true;
+	buttonStop.disabled = false;
+
+	timerId = setInterval(() => {
+		document.body.style.backgroundColor = getRandomHexColor();
+	}, 1000)
+}
+
+function onButtonStopClick() {
+	clearInterval(timerId);
+
+	buttonStart.disabled = false;
+	buttonStop.disabled = true;
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
