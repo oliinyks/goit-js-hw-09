67c664247822c52bd1b471cd9388de378d9d2057
Olijnyks <olijnyks@ukr.net>
2022-08-12 09:45:54 +0300

    add task 3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,41 @@
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+const formEl = document.querySelector('.form');
+  
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+  return new Promise((resolve, reject) => {
+    setInterval(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
+}
+
+formEl.addEventListener('submit', onButtenClick);
+
+function onButtenClick(event) {
+  event.preventDefault();
+
+  const {
+    elements: { delay, step, amount },
+  } = event.currentTarget;
+
+   let delayEl = Number(delay.value);
+   const stepEl = Number(step.value);
+   const amounEl = Number(amount.value);
+
+	for (let i = 1; i <= amounEl; i+=1) {
+    createPromise(i, delayEl)
+      .then(({ position, delay }) => {
+        Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+      });
+    delayEl += stepEl;
   }
 }
