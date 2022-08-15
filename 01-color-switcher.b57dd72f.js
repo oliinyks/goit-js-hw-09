006d05d1876e776362538a1616a70592f693dc79
oliinyks <101010916+oliinyks@users.noreply.github.com>
2022-08-15 08:02:27 +0000

    Deploying to gh-pages from @ oliinyks/goit-js-hw-09@96a9126d97b5ab143fed8bc6c189ba52c17a546d 🚀

--- 01-color-switcher.b57dd72f.js
+++
@@ -1,2 +0,0 @@
-const e=document.querySelector("[data-start]"),t=document.querySelector("[data-stop]");let d=null;t.disabled=!0,e.addEventListener("click",(function(){e.disabled=!0,t.disabled=!1,d=setInterval((()=>{document.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`}),1e3)})),t.addEventListener("click",(function(){clearInterval(d),e.disabled=!1,t.disabled=!0}));
-//# sourceMappingURL=01-color-switcher.b57dd72f.js.map
